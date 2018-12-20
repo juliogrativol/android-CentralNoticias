19d1deee88277cf94e234f1d3c74939a98b8a9a3
jthomaz <jthomaz@brasilcap.com.br>
2018-12-20 15:58:55 -0200

    Criando Frame na tela de About

--- app/src/main/java/com/njinformatica/centralnoticias/AboutActivity.java
+++ app/src/main/java/com/njinformatica/centralnoticias/AboutActivity.java
@@ -1,12 +1,13 @@
 package com.njinformatica.centralnoticias;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -20,6 +21,41 @@ public class AboutActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        Button sobreAppButton = findViewById(R.id.btn_sobre_app);
+        Button sobreAutorButton = findViewById(R.id.btn_sobre_autor);
+
+        sobreAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make new fragment to show this selection.
+                FragmentManager fm = getSupportFragmentManager();
+
+                // Execute a transaction, replacing any existing fragment
+                // with this one inside the frame.
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                ft.replace(R.id.about_frame, new AboutAppFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+
+        sobreAutorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make new fragment to show this selection.
+                FragmentManager fm = getSupportFragmentManager();
+
+                // Execute a transaction, replacing any existing fragment
+                // with this one inside the frame.
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+                ft.replace(R.id.about_frame, new AboutAutorFragment());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
